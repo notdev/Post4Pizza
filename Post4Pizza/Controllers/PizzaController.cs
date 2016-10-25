95f446e38174cdc53f613e8388ea652ed2a018fe
notdev <notdev@users.noreply.github.com>
2016-10-25 12:30:31 +0200

    Add some error handling and swagger

--- Post4Pizza/Controllers/PizzaController.cs
+++ Post4Pizza/Controllers/PizzaController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using Post4Pizza.Exceptions;
 using Post4Pizza.PizzaProviders;
 
 namespace Post4Pizza.Controllers
@@ -10,20 +13,40 @@ namespace Post4Pizza.Controllers
     {
         [HttpPost]
         [Route("Api/OrderPizza")]
-        public void OrderPizza(string pizzaProviderName, string userName, string password, List<string> pizzasToOrder)
+        public HttpResponseMessage OrderPizza(string pizzaProviderName, string userName, string password, List<string> pizzasToOrder)
         {
             var provider = GetPizzaProvider(pizzaProviderName);
-            provider.OrderPizza(userName, password, pizzasToOrder);
+            if (provider == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest) {Content = new StringContent($"Failed to order. Did not find provider '{pizzaProviderName}'")};
+            }
+
+            try
+            {
+                provider.OrderPizza(userName, password, pizzasToOrder);
+            }
+            catch (PizzaProviderException ex)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest) {Content = new StringContent($"Failed to order. Error: '{ex.Message}'")};
+            }
+            catch (Exception ex)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError) {Content = new StringContent($"Unhandled error occured. Error: '{ex}'")};
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
         private IPizzaProvider GetPizzaProvider(string providerName)
         {
-            var type = typeof(IPizzaProvider);
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p));
-            var pizzaProviders = types as IEnumerable<IPizzaProvider>;
-            return pizzaProviders.FirstOrDefault(p => p.ProviderName == providerName);
+            var pizzaProviderInterfaces = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(t => t.GetInterfaces().Contains(typeof(IPizzaProvider)));
+
+            var modules = pizzaProviderInterfaces.Select(type => (IPizzaProvider) Activator.CreateInstance(type));
+
+            var provider = modules.FirstOrDefault(m => m.ProviderName == providerName);
+            return provider;
         }
     }
 }
