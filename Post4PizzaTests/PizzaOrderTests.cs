0362855b5a399f0dc9097d36e8ffc594113f1a83
Dusan Obert <obert.dusan@gmail.com>
2017-04-24 18:34:31 +0200

    Readd test

---
+++ Post4PizzaTests/PizzaOrderTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Post4Pizza.PizzaProviders;
+
+namespace Post4PizzaTests
+{
+    [TestClass]
+    public class PizzaOrderTests
+    {
+        [TestMethod]
+        public void OrderPizzaTest()
+        {
+            var order = new CarusoPizzaProvider();
+            order.OrderPizza("user@mail.com", "password", new List<string> { "Margherita" });
+        }
+    }
+}
